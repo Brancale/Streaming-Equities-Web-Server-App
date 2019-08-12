a473555ab027c71108d9fbda8dba123559d3a053
James Brancale <james.brancale@gmail.com>
2019-08-12 22:00:38 +0100

    Login form with action button
    Login form accepts data and posts it to page with alert

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import React from 'react';
 import MyComponent from './MyComponent';
 
-
-
-
-//username = document.getElementById("name").value; 
+//username = document.getElementById("name").value;
 //password = document.getElementById("password").value;
 
 //alert(username);
 //alert(password);
 
 
+
 function App() {
- return ( 
-  <form>
-  <label>
-      Username:  
-      <input type="text"id="name" name="username" />
-      <br></br>
-      Password:
-      <input type="password" id="password" name="password" />
-      </label>
-      <br></br>
-      <button id="submit">Submit</button>
-    </form>
+ return (
+  <MyComponent/>
   );
 }
 
+//loginForm.addEventListener("submit", function(e) {
 
-document.getElementById("form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("do you need a hug");
-  });
+  //alert("do you need a hug");
+//});
 
 export default App;
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 
 
+function submitLogin() {
+  var user = document.getElementById("username");
+  var pwd = document.getElementById("password");
+  document.getElementById('testMessage').innerHTML = "Username: "+ user.value + " Password: : " + pwd.value;
+  alert("Success");
+}
+
 const MyComponent = () => {
- return ( 
-    
+ return (
+
     <div id= "formforlogin">
         <h1>Log In to Database</h1>
         <form>
-        <label>
-            Username:  
-            <input type="text"id="name" name="username" />
+          <label>
+            Username:
+            <input type="text"id="username" name="username" />
             <br></br>
             Password:
             <input type="password" id="password" name="password" />
-            </label>
-            <br></br>
-            <button id="submit">Submit</button>
-          </form>
+          </label>
+          <br></br>
+          <button id="submit" onClick={submitLogin}>Submit</button>
+          <div id="testMessage"></div>
+        </form>
     </div>
       );
     };
