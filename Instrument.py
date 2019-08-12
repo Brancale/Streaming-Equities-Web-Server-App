4c87ca9fdd0770910861fb5f8308a2986a582d83
jbrancale <james.brancale@gmail.com>
2019-08-12 10:20:40 +0100

    InstrumentAlgo

--- Instrument.py
+++ Instrument.py
@@ -1,3 +1,5 @@
+import numpy
+
 class Instrument:
     instrumentName = None
     hashedValue = 0
@@ -14,10 +16,13 @@ class Instrument:
         self.drift = drift
         self.variance = variance
 
-    def calculateNextPrice(type):
-        if (type == 'S'):
-
-        else: # type is B
+    def calculateNextPrice(self, direction):
+        newPriceStarter = self.__price + numpy.random.normal(0, 1) * self.__variance + self.__drift
+        newPrice = newPriceStarter if (newPriceStarter > 0) else 0.0
+        if self.__price < self.__startingPrice * 0.4:
+            self.__drift = (-0.7 * self.__drift)
+        self.__price = newPrice * 1.01 if direction == 'B' else newPrice * 0.99
+        return self.__price
 
 
 
