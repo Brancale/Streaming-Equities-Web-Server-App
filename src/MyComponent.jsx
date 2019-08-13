1cb6dbf1cb16413c3552dfaf8de61b210fbe261d
AlexDavydov1995 <32180785+AlexDavydov1995@users.noreply.github.com>
2019-08-13 11:13:31 +0200

    Add files via upload

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 
+var user = "";
+var pwd = "";
+
+// Get
+const getJSON = async () => {
+
+  const res1 = await axios.get('http://127.0.0.1:5001/sendjson');
+      const reply = await res1.data;
+      console.log(reply)
+      alert(reply);
+}
+
+// Send
+const sendJSON = async () => {
+
+  var data = JSON.stringify({
+    "username": user.value,
+    "password": pwd.value
+  });
+
+  var request = new XMLHttpRequest();
+  request.open('POST', 'http://127.0.0.1:5001/recieveJSON', true);
+  request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  request.send(data);
+}
 
 function submitLogin(e) {
   e.preventDefault();
-  var user = document.getElementById("username");
-  var pwd = document.getElementById("password");
+  
+  user = document.getElementById("username");
+  pwd = document.getElementById("password");
   document.getElementById('testMessage').innerHTML = "Username: "+ user.value + " Password: : " + pwd.value;
-  alert("Success");
+  sendJSON();
+  getJSON();
+  //alert("Success");
 }
 
 const MyComponent = () => {
