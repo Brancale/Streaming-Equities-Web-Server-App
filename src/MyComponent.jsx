52269da8cf9a37b9e7bdca6c4b23ee55ca04a2e9
Julie Semchenkova <jools.semchenkova@gmail.com>
2019-08-13 13:07:37 +0100

    Post the pwd and user name

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -4,27 +4,47 @@ import axios from 'axios';
 var user = "";
 var pwd = "";
 
-// Get
-const getJSON = async () => {
 
-  const res1 = await axios.get('http://127.0.0.1:5001/sendjson');
-      const reply = await res1.data;
-      console.log(reply)
-      alert(reply);
-}
+// Get
+// const getJSON = async () => {
+
+//   const res1 = await axios.get('http://127.0.0.1:5001/login');
+//       const reply = await res1.data;
+//       console.log(reply)
+//       alert(reply);
+// }
 
 // Send
-const sendJSON = async () => {
+// const sendJSON = async () => {
 
-  var data = JSON.stringify({
+//   var data = JSON.stringify({
+//     "username": user.value,
+//     "password": pwd.value
+//   });
+
+//   var request = new XMLHttpRequest();
+//   request.open('POST', 'http://127.0.0.1:5001/login', true);
+//   request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+//   var result = request.send(data);
+//   console.log(result)
+// }
+
+
+async function makePostRequest() {
+
+  var data = {
     "username": user.value,
     "password": pwd.value
-  });
-
-  var request = new XMLHttpRequest();
-  request.open('POST', 'http://127.0.0.1:5001/recieveJSON', true);
-  request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-  request.send(data);
+  };
+  axios.post('http://127.0.0.1:5001/login', data)
+    .then((response) => {
+      console.log(response);
+      alert('authorised')
+    })
+    .catch((error) => {
+      console.log(error);
+      alert('go to hell')
+    });
 }
 
 function submitLogin(e) {
@@ -33,8 +53,9 @@ function submitLogin(e) {
   user = document.getElementById("username");
   pwd = document.getElementById("password");
   document.getElementById('testMessage').innerHTML = "Username: "+ user.value + " Password: : " + pwd.value;
-  sendJSON();
-  getJSON();
+  makePostRequest()
+ // sendJSON();
+  //getJSON();
   //alert("Success");
 }
 
