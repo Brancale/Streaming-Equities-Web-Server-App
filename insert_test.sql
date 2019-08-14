99ef2b95fdeced33a08257e085c781caee6cd1e1
Julie Semchenkova <jools.semchenkova@gmail.com>
2019-08-14 08:57:07 +0100

    Role and User updates

--- insert_test.sql
+++ insert_test.sql
@@ -8,6 +8,9 @@ INSERT INTO `mydb`.`Deals`(`dealId`, `instrumentName`, `cpty`, `price`, `type`, 
 (20011, "Heliosphere", "Lewis", 0.0, "S",1, "2019-08-11 14:20:16");
 SELECT * FROM Deals;
 
-INSERT INTO `mydb`.`Users`(`userId`, `password`) VALUES
-("Jools", "pwd"), ("Inan", "pwd"), ("Reuben", "pwd"), ("Alex", "pwd"), ("James", "pwd"), ("Amy", "pwd");
-SELECT * FROM Users;
+INSERT INTO `mydb`.`Roles`(`roleId`, `roleName`) VALUES
+(1, "Admin"), (2, "Trader"), (3, "Senior Trader");
+
+INSERT INTO `mydb`.`Users`(`userId`, `password`, `roleId`) VALUES
+("Jools", "pwd", 1), ("Inan", "pwd", 2), ("Reuben", "pwd", 3), ("Alex", "pwd", 1), ("James", "pwd", 2), ("Amy", "pwd", 3);
+SELECT * FROM Users INNER JOIN Roles ON Users.roleId = Roles.roleId;
