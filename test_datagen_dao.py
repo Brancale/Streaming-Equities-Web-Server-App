e66c3a115e63f5e915575229c01c3a03cffc5b97
James Brancale <james.brancale@gmail.com>
2019-08-14 13:28:44 +0100

    Repaired authorization tests. TODO modify for type

--- test_datagen_dao.py
+++ test_datagen_dao.py
@@ -13,10 +13,14 @@ class test_dao(unittest.TestCase):
         userObj = datagen_dao.as_user(loginJSON)
         assert userObj.password == "pass", "Should return pass"
 
-    def test_auth(self):
+    def test_auth_fail(self):
         loginJSON = {"username":"user","password":"pass"}
-        datagen_dao.login()
-        response = requests.post("http://127.0.0.1:5000", json=loginJSON)
+        response = requests.post("http://127.0.0.1:5000/login", json=loginJSON)
+        assert response.status_code == 401
+
+    def test_auth_good(self):
+        loginJSON = {"username":"James","password":"pwd"}
+        response = requests.post("http://127.0.0.1:5000/login", json=loginJSON)
         assert response.status_code == 200
 
 
